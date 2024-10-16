1134d79ef0428a1aa903943900b2bae54068862e
dineifernandes <dinei_aparecido@hotmail.com>
2024-10-16 14:55:52 -0300

    update view details student

--- lib/app/modules/student/student_detail_view.dart
+++ lib/app/modules/student/student_detail_view.dart
@@ -36,76 +36,79 @@ class StudentDetailsView extends StatelessWidget {
             const SizedBox(height: 20), // Espaço entre a imagem e o card
 
             // Card com as observações do aluno
-            Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Observações do aluno:',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            Expanded(
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Observações do aluno',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const Divider(),
-                    // Verificar se há observações
-                    student.observations != null &&
-                            student.observations!.isNotEmpty
-                        ? SizedBox(
-                            height: 260, // Limite de altura para o ListView
-                            child: ListView.builder(
-                              itemCount: student.observations!.length,
-                              itemBuilder: (context, index) {
-                                final observation =
-                                    student.observations![index];
-                                final id = observation.id!;
-                                return ListTile(
-                                  trailing: IconButton(
-                                    onPressed: () async {
-                                      var mensagem = await controller
-                                          .removeObservation(id);
-                                      Get.back();
-                                      if (mensagem['code'] != null &&
-                                          mensagem['code'] == 0) {
-                                        Get.snackbar(
-                                            'Sucesso', mensagem['objeto'],
-                                            backgroundColor: Colors.green,
-                                            colorText: Colors.white);
-                                        controller.getStudents();
-                                      } else {
-                                        Get.snackbar(
-                                            'Falha', mensagem['objeto'],
-                                            backgroundColor: Colors.red,
-                                            colorText: Colors.white);
-                                      }
-                                    },
-                                    icon: Icon(
-                                      Icons.remove,
-                                      color: Colors.red.shade700,
+                      const Divider(),
+                      // Verificar se há observações
+                      student.observations != null &&
+                              student.observations!.isNotEmpty
+                          ? SizedBox(
+                              height: Get.width *
+                                  .8, // Limite de altura para o ListView
+                              child: ListView.builder(
+                                itemCount: student.observations!.length,
+                                itemBuilder: (context, index) {
+                                  final observation =
+                                      student.observations![index];
+                                  final id = observation.id!;
+                                  return ListTile(
+                                    trailing: IconButton(
+                                      onPressed: () async {
+                                        var mensagem = await controller
+                                            .removeObservation(id);
+                                        Get.back();
+                                        if (mensagem['code'] != null &&
+                                            mensagem['code'] == 0) {
+                                          Get.snackbar(
+                                              'Sucesso', mensagem['objeto'],
+                                              backgroundColor: Colors.green,
+                                              colorText: Colors.white);
+                                          controller.getStudents();
+                                        } else {
+                                          Get.snackbar(
+                                              'Falha', mensagem['objeto'],
+                                              backgroundColor: Colors.red,
+                                              colorText: Colors.white);
+                                        }
+                                      },
+                                      icon: Icon(
+                                        Icons.remove,
+                                        color: Colors.red.shade700,
+                                      ),
                                     ),
-                                  ),
-                                  contentPadding: EdgeInsets.zero,
-                                  title: Text(
-                                    observation.observacao?.toUpperCase() ??
-                                        'OBSERVAÇÃO SEM DESCRIÇÃO',
-                                    style: const TextStyle(
-                                        fontSize: 14, fontFamily: 'Poppins'),
-                                  ),
-                                );
-                              },
+                                    contentPadding: EdgeInsets.zero,
+                                    title: Text(
+                                      observation.observacao?.toUpperCase() ??
+                                          'OBSERVAÇÃO SEM DESCRIÇÃO',
+                                      style: const TextStyle(
+                                          fontSize: 14, fontFamily: 'Poppins'),
+                                    ),
+                                  );
+                                },
+                              ),
+                            )
+                          : const Text(
+                              'Nenhuma observação disponível.',
+                              style: TextStyle(fontSize: 16),
                             ),
-                          )
-                        : const Text(
-                            'Nenhuma observação disponível.',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
